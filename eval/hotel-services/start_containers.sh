9b5511f0cd4bc378f13eb36510bc131904bc20ad
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-10-04 06:29:55 -0400

    feat: local containers scripts

--- eval/hotel-services/start_containers.sh
+++ eval/hotel-services/start_containers.sh
@@ -1,5 +1,6 @@
 WORKDIR=`dirname $(realpath $0)`
 cd $WORKDIR
-docker-compose -f docker-compose-profile.yml -H "ssh://root@danyang-05" up -d
-docker-compose -f docker-compose-geo.yml -H "ssh://root@danyang-06" up -d
-docker-compose -f docker-compose-rate.yml -H "ssh://root@danyang-06" up -d
+docker-compose -f docker-compose-profile.yml up -d
+docker-compose -f docker-compose-geo.yml up -d
+docker-compose -f docker-compose-rate.yml up -d
+docker-compose -f docker-compose-services.yml up -d
--- eval/hotel-services/stop_containers.sh
+++ eval/hotel-services/stop_containers.sh
@@ -1,5 +1,6 @@
 WORKDIR=`dirname $(realpath $0)`
 cd $WORKDIR
-docker-compose -f docker-compose-profile.yml -H "ssh://root@danyang-05" down
-docker-compose -f docker-compose-geo.yml -H "ssh://root@danyang-06" down 
-docker-compose -f docker-compose-rate.yml -H "ssh://root@danyang-06" down
+docker-compose -f docker-compose-profile.yml down
+docker-compose -f docker-compose-geo.yml down 
+docker-compose -f docker-compose-rate.yml down
+docker-compose -f docker-compose-services.yml down
