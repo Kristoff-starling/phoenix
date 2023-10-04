f842e37e9732cff0e125716ec9431218a33077be
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-10-04 13:56:32 -0400

    feat: run all containers on the same host

--- eval/hotel-services/start_containers.sh
+++ eval/hotel-services/start_containers.sh
@@ -1,6 +1,6 @@
 WORKDIR=`dirname $(realpath $0)`
 cd $WORKDIR
-docker-compose -f docker-compose-profile.yml up -d
-docker-compose -f docker-compose-geo.yml up -d
-docker-compose -f docker-compose-rate.yml up -d
+# docker-compose -f docker-compose-profile.yml up -d
+# docker-compose -f docker-compose-geo.yml up -d
+# docker-compose -f docker-compose-rate.yml up -d
 docker-compose -f docker-compose-services.yml up -d
--- eval/hotel-services/stop_containers.sh
+++ eval/hotel-services/stop_containers.sh
@@ -1,6 +1,6 @@
 WORKDIR=`dirname $(realpath $0)`
 cd $WORKDIR
-docker-compose -f docker-compose-profile.yml down
-docker-compose -f docker-compose-geo.yml down 
-docker-compose -f docker-compose-rate.yml down
+# docker-compose -f docker-compose-profile.yml down
+# docker-compose -f docker-compose-geo.yml down 
+# docker-compose -f docker-compose-rate.yml down
 docker-compose -f docker-compose-services.yml down
