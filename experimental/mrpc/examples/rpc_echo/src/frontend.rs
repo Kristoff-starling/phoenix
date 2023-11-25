9542fbdb4bc6ddf5d6811d01e180ca406cc94c1d
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-11-25 12:47:02 -0500

    fix: rpc_echo frontend

--- experimental/mrpc/examples/rpc_echo/src/frontend.rs
+++ experimental/mrpc/examples/rpc_echo/src/frontend.rs
@@ -7,33 +7,122 @@ pub mod rpc_echo {
 }
 
 use rpc_echo::greeter_client::GreeterClient;
-use rpc_echo::HelloRequest;
+use rpc_echo::{HelloRequest, HelloReply};
 use std::{
+    convert::Infallible,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    net::{SocketAddr, TcpListener, TcpStream},
     thread,
 };
+use tokio::runtime::Runtime;
+
+use crossbeam::channel::{unbounded, Sender, Receiver};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Server, Body, Request, Response, StatusCode};
+
+#[derive(Debug)]
+pub enum Command {
+    Req {
+        req: HelloRequest,
+        resp: Sender<HelloReply>,
+    }
+}
 
 // The main function that starts the server and listens for incoming connections.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let (tx, rx) = unbounded();
 
-    // Loop over incoming connections.
-    for stream in listener.incoming() {
-        let stream = stream?;
-
-        // Spawn a new thread to handle the connection.
-        thread::spawn(|| {
-            handle_connection(stream).unwrap_or_else(|error| {
-                eprintln!("Error handling connection: {}", error);
-            });
+    thread::spawn(|| {
+        send_proxy(rx).unwrap_or_else(|error| {
+            eprintln!("Error sending request: {}", error);
         });
-    }
+    });
+
+    Runtime::new().unwrap().block_on(async {
+        let make_service = make_service_fn(move |_conn| {
+            let tx = tx.clone();
+            let service = service_fn(move |req| handle_request(req, tx.clone()));
+            async move { Ok::<_, Infallible>(service) }
+        });
+
+        let addr = SocketAddr::from(([0, 0, 0, 0], 7878));
+        let server = Server::bind(&addr).serve(make_service);
+        if let Err(e) = server.await {
+            eprintln!("Server error: {:?}", e);
+        }
+    });
+
     Ok(())
+
+    // let listener = TcpListener::bind("0.0.0.0:7878")?;
+
+    // // Loop over incoming connections.
+    // for stream in listener.incoming() {
+    //     let stream = stream?;
+    //     let tx = tx.clone();
+
+    //     // Spawn a new thread to handle the connection.
+    //     thread::spawn(|| {
+    //         handle_connection(stream, tx).unwrap_or_else(|error| {
+    //             eprintln!("Error handling connection: {}", error);
+    //         });
+    //     });
+    // }
+    // Ok(())
+}
+
+fn send_proxy(rx: Receiver<Command>) -> Result<(), Box<dyn std::error::Error>> {
+    let client = GreeterClient::connect("rpc_echo_server:5000")?;
+
+    loop {
+        let cmd = rx.recv();
+        match cmd {
+            Ok(Command::Req { req, resp }) => {
+                let hello_reply = smol::block_on(client.say_hello(req))?;
+                let _ = resp.send(hello_reply.as_ref().clone());
+            }
+            Err(_) => {
+                eprintln!("Command error {:?}", cmd);
+            }
+        }
+    }
+}
+
+async fn handle_request(
+    request: Request<Body>,
+    tx: Sender<Command>
+) -> Result<Response<Body>, hyper::Error> {
+    let uri = request.uri().path();
+    match uri {
+        "/apple" | "/banana" => {
+            let req = HelloRequest { name: uri.into() };
+            let (resp_tx, resp_rx) = unbounded();
+            let cmd = Command::Req {
+                req: req,
+                resp: resp_tx,
+            };
+            if tx.send(cmd).is_err() {
+                eprintln!("channel error");
+            }
+            let reply = resp_rx.recv().unwrap();
+            let reply_msg = String::from_utf8_lossy(&reply.message);
+            let msg = format!("{reply_msg}");
+            let response = Response::builder()
+                .status(200)
+                .body(msg.into()).unwrap();
+            Ok(response)
+        }
+        _ => {
+            eprintln!("Not found: {:?}", request);
+            let mut not_found = Response::new(Body::empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 // Function to handle the connection, read the request, and send the response.
-fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+fn handle_connection(mut stream: TcpStream, tx: Sender<Command>) -> Result<(), Box<dyn std::error::Error>> {
     let mut buf_reader = BufReader::new(&mut stream);
     let mut request_str = String::new();
     buf_reader.read_line(&mut request_str)?;
@@ -43,10 +132,17 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Er
     let uri = request_parts[1];
 
     // Connect to the Greeter service and send the HelloRequest.
-    let client = GreeterClient::connect("localhost:5000")?;
     let req = HelloRequest { name: uri.into() };
-    let reply = smol::block_on(client.say_hello(req))?;
-    println!("reply: {}", String::from_utf8_lossy(&reply.message));
+
+    let (resp_tx, resp_rx) = unbounded();
+    let cmd = Command::Req {
+        req: req,
+        resp: resp_tx,
+    };
+    if tx.send(cmd).is_err() {
+        eprintln!("channel error");
+    }
+    let reply = resp_rx.recv().unwrap();
 
     // Prepare and send the HTTP response.
     let status_line = "HTTP/1.1 200 OK";
